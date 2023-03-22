03dcc93f7d99a745225490f90f603f4348442ea9
OmkarSunilSurve <108228241+OmkarSunilSurve@users.noreply.github.com>
2023-03-23 02:10:45 +0530

    Add files via upload

---
+++ Data_Cleaning.sql
@@ -0,0 +1,204 @@
+Create database if not exists Data_Cleaning;
+use Data_cleaning;
+
+drop table if exists data_cleaning;
+CREATE TABLE data_cleaning (
+	UniqueID DECIMAL(38,0) NOT NULL, 
+	ParcelID VARCHAR(40) NOT NULL, 
+	LandUse VARCHAR(40) NOT NULL, 
+	PropertyAddress VARCHAR(40), 
+	SaleDate DATE , 
+	SalePrice DECIMAL(38,0) NOT NULL, 
+	LegalReference VARCHAR(40) NOT NULL, 
+	SoldAsVacant VARCHAR(5) NOT NULL, 
+	OwnerName VARCHAR(40), 
+	OwnerAddress VARCHAR(40), 
+	Acreage DECIMAL(38,3), 
+	TaxDistrict VARCHAR(40), 
+	LandValue DECIMAL(38,3), 
+	BuildingValue DECIMAL(38,3), 
+	TotalValue DECIMAL(38,3), 
+	YearBuilt DECIMAL(38,3), 
+	Bedrooms DECIMAL(38,3), 
+	FullBath DECIMAL(38,3), 
+	HalfBath DECIMAL(38,3)
+);
+
+set session sql_mode = ' ';
+LOAD DATA INFILE 
+'c:/data_cleaning.csv'
+into table `data_cleaning`
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+select * from data_cleaning limit 1000;
+
+SELECT SoldAsVacant , count(SoldAsVacant)
+from data_cleaning
+group by SoldAsVacant ; -- Thus indicates that we have to tranform this data to contain uniform format i.i with yes,no or y,n
+
+
+
+
+
+-- 1. Populate Property Address of Data
+
+select * from data_cleaning 
+where PropertyAddress = '';
+
+
+-- we are trying to get reed of data with no property address using self join method
+
+select b.PropertyAddress
+ from data_cleaning a
+join data_cleaning b
+ on a.ParcelID = b.ParcelID
+  and a.UniqueID <> b.UniqueID
+where a.PropertyAddress = '';
+
+update data_cleaning a 
+left join data_cleaning b 
+on a.ParcelID = b.ParcelID 
+and a.UniqueId <> b.UniqueID
+set a.PropertyAddress = b.PropertyAddress
+where a.PropertyAddress = '';
+
+
+-- 2. Breaking Address into individual columns (Address,city,state)
+
+-- we make use of locate function to help us separating this
+
+
+select 
+substring(PropertyAddress,1,locate(',',PropertyAddress)-1) as address,
+substring(PropertyAddress,locate(',',PropertyAddress) + 1,length(PropertyAddress)) as city
+from data_cleaning;
+
+-- we will create 2 new columns to hold these values
+
+alter table data_cleaning
+add column PropAddress varchar(50),
+add column PropCity varchar(60);
+
+update data_cleaning
+set PropAddress = substring(PropertyAddress,1,locate(',',PropertyAddress)-1) ;
+
+update data_cleaning
+set PropCity = substring(PropertyAddress,locate(',',PropertyAddress) + 1,length(PropertyAddress));
+
+
+-- Doing same for owner adress column
+-- Here we will step by step alot the address as here we have 2 delimiters
+
+alter table data_cleaning 
+add column OwnerAdd varchar(50),
+add column OwnerCity Varchar(50),
+add column OwnerState varchar(20);
+
+select * from data_cleaning limit 10;
+
+update data_cleaning
+set OwnerAdd = substring(OwnerAddress,1,locate(',',OwnerAddress) -1 );
+
+-- here we create proxy city so that we could break this to get our city for owner
+
+alter table data_cleaning
+add column proxyCity varchar(50),
+add column ProxyState varchar(50);
+
+select * from data_cleaning limit 19;
+
+-- here we create proxy city to extract city and state for our work
+
+
+update data_cleaning
+set proxyCity = substring(OwnerAddress , locate(',',OwnerAddress)+1,length(OwnerAddress));
+
+update data_cleaning
+set OwnerCity = substring(proxyCity ,1, locate(',',proxyCity)-1);
+
+update data_cleaning
+set OwnerState = substring(proxyCity , locate(',',proxyCity)+1,length(proxyCity));
+
+
+-- 3. change Y and N in SoldAsVacant to Yes and NO
+
+-- first we check what is the count of different type of values in this column
+
+SELECT SoldAsVacant , count(SoldAsVacant)
+from data_cleaning
+group by SoldAsVacant
+order by 2 ;
+
+select SoldAsVacant ,
+	case when SoldAsVacant = 'Y' then 'Yes'
+		when SoldAsVacant = 'N' then 'No'
+		else SoldAsVacant
+	end
+from data_cleaning;
+
+update data_cleaning
+set SoldAsVacant = case when SoldAsVacant = 'Y' then 'Yes'
+	when SoldAsVacant = 'N' then 'No'
+    else SoldAsVacant
+    end;
+
+
+-- 4. Remove Duplicates and Null value present at various fields .
+-- we make use of CTE to help us find duplicate entries. Here we had to do inner join between our table and the cte that we are making
+-- for deletion
+
+with CTE as(
+select UniqueID,
+row_number() over (
+partition by ParcelID,
+PropertyAddress,
+SalePrice,
+SaleDate,
+LegalReference
+order by UniqueId ) row_num
+from data_cleaning 
+)
+Delete ddc from data_cleaning ddc 
+inner join CTE c on c.UniqueID = ddc.UniqueID
+where row_num > 1 ;
+
+
+-- deletion of NUll values.
+-- here we observe that Field OwnerName contains some blank places. we will remove those as they wont be contibuting to our analysis
+
+delete from data_cleaning where OwnerName = '' ;
+
+
+-- 5 . Delete Unused Columns that were present or we created for our purpose
+
+select * from data_cleaning ;
+
+-- upon inspection i did find 2 column which were created early i.e proxycity , proxystate .
+-- As we have created proper address for property address and owner address , now we delete the original column for these two .
+-- we also delete tax district as we dont have any use of it.
+
+alter table data_cleaning 
+drop column proxyCity ,
+drop column ProxyState ,
+drop column PropertyAddress,
+drop column OwnerAddress ,
+drop column TaxDistrict;
+
+-- deletion is now complete
+
+
+
+-------------------------------------------------- Here we have completed cleaning our data ------------------------------------------ 
+
+
+
+
+
+
+
+
+
+
---
+++ SIR_Model.jl
@@ -0,0 +1,94 @@
+using DelimitedFiles;
+using Plots;
+gr();
+using Dates;
+
+DATA = readdlm("EbolaVirus.csv",',');
+col1 = DATA[:,1];
+for i = 1 : length(col1)
+	col1[i] = Dates.DateTime(col1[i],"d u y")
+end
+col1;
+
+Ndays(x) = Dates.datetime2rata(x)-Dates.datetime2rata(col1[54]);
+
+epidays = Array{Int64}(undef,54);
+
+for i = 1 : 54
+	epidays[i] = Ndays(col1[i])
+end
+
+epidays;
+
+DATA[:,1] = epidays;
+
+DATA;
+
+##DelimitedFiles.writedlm("Julia_SIR_MODEL.csv",DATA,',');
+
+DATA[end-9:end,:];
+
+row,col = size(DATA)
+
+for i = 1 : row
+	for j = 1 : col
+		if !isnumeric(string(DATA[i,j])[1])
+		    DATA[i,j] = 0
+	   	end
+	end
+end
+
+DelimitedFiles.writedlm("Julia_SIR_MODEL.csv",DATA,',');
+
+DATA = DelimitedFiles.readdlm("Julia_SIR_MODEL.csv",',');
+
+No_inf = DATA[:,2];
+
+#plot(epidays,No_inf);
+
+diff_data = DATA[:,[4,6,8]];
+
+#plot(epidays,diff_data);
+
+#plot!(epidays,diff_data,marker = ([:diamond :square :star7],4),line = :scatter , xlabel = " No of days since 22nd March " , ylabel = "No of Cases",title = "Ebola Cases in different Countries",label = ["Guinea" "Liberia" "Sierra Leone"])
+
+function updateSIR(vec)
+	sus = vec[1]
+	inf = vec[2]
+	rem = vec[3]
+	newS = sus - lambda*sus*inf*dt
+	newI = inf + lambda*sus*inf*dt - gamma*inf*dt
+	newR = rem + gamma*inf*dt
+	return [newS newI newR]
+end
+
+lambda = 1.47e-6 ; gamma = 1/8 ; dt = 0.5 ; tfin = 610 ; s0 = 10^5 ; i0 = 20. ; r0 = 0 ;
+
+nsteps = round(Int64,tfin/dt);
+
+resultsVal = Array{Float64}(undef , nsteps + 1 , 3);
+
+resultsVal[1,:] = [s0 i0 r0];
+
+timevec = Array{Float64}(undef,nsteps+1);
+
+timevec[1] = 0 ;
+
+for i = 1 : nsteps
+	resultsVal[i+1,:] = updateSIR(resultsVal[i,:])
+	timevec[i+1] = timevec[i] + dt
+end
+#plot(timevec,resultsVal,leg = :topright,xlabel = "Time",ylabel = "Population",Title = "SIR Model",label = ["Sus" "Inf" "Rem"])
+
+rVal = resultsVal[:,3];
+iVal = resultsVal[:,2];
+cVal = iVal + rVal;
+
+plot(timevec,cVal,xlabel = "Epidemic day" , ylabel = "No of Cases to date", title = "Model VS Data",label = "Model Values")
+
+tval_data = DATA[:,1];
+tot_cases_data = DATA[:,2];
+
+plotA = plot!(tval_data,tot_cases_data,label = "Reported Cases",legend = :right,line=:scatter)
+
+savefig(plotA,"SIR_MODEL_EBOLA-VIRUS.png")
